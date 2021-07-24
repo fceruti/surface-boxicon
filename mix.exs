16ecdde4160c1d6e179b45b4bf64ef0c0bdff1d2
Francisco Ceruti <hello@fceruti.com>
2021-07-24 19:40:16 -0400

    add mix task boxicon.gen.icons

--- mix.exs
+++ mix.exs
@@ -22,8 +22,6 @@ defmodule Boxicon.MixProject do
   defp deps do
     [
       {:surface, "~> 0.5.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
