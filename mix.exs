183c3426313a734c81f66375b2aceda4f4b86b7b
Francisco Ceruti <hello@fceruti.com>
2021-07-24 22:36:16 -0400

    add ex_docs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Boxicon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Surface Boxicons",
+      source_url: "https://github.com/fceruti/surface-boxicons",
+      docs: [
+        # The main page in the docs
+        main: "Boxicon",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -21,7 +30,8 @@ defmodule Boxicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:surface, "~> 0.5.1"}
+      {:surface, "~> 0.5.1"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
