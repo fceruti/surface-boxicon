6059518f75fd2677a90636e0aa1d4f2d30fc1468
Francisco Ceruti <hello@fceruti.com>
2021-07-25 13:53:37 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Boxicon.MixProject do
   def project do
     [
       app: :boxicon,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
