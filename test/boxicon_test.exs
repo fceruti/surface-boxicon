768429e1efeb4ca6784cc2e4932ae95eee2fe774
Francisco Ceruti <hello@fceruti.com>
2021-07-29 17:25:01 -0400

    add Boxicon test

--- test/boxicon_test.exs
+++ test/boxicon_test.exs
@@ -1,8 +1,29 @@
 defmodule BoxiconTest do
   use ExUnit.Case
-  doctest Boxicon
+  use Surface.LiveViewTest
 
-  test "greets the world" do
-    assert Boxicon.hello() == :world
+  # The default endpoint for testing
+  @endpoint Endpoint
+
+  test "" do
+    html =
+      render_surface do
+        ~F"""
+          <Boxicon
+            type="regular"
+            name="calendar"
+            size="24"
+            class="icon"
+          />
+
+        """
+      end
+
+    assert String.contains?(html, "M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0")
+    assert String.contains?(html, "class=\"")
+    assert String.contains?(html, "height=\"")
+    assert String.contains?(html, "width=\"")
+    assert String.contains?(html, "<svg")
+    assert String.contains?(html, "</svg>")
   end
 end
